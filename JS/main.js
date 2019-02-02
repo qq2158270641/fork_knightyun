204892f543e4cf5129caad0f9faceeb02e8263ed
qq2158270641 <2158270641@qq.com>
2019-02-02 16:40:20 +0800

    add test

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+/* exported playZero */
+/* exported playOver */
+
+function playZero() {
+	"use strict";
+    alert("已是第一集。");
+	
+}
+
+function playOver() {
+	"use strict";
+	alert("已是最后一集。");
+}
